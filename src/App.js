e6d0530693ecb5bf6c082d9083b220de8e5ff7ed
Elvira Gizatullina <elvirawed@gmail.com>
2023-03-06 12:09:10 -0800

    Starter file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
 
 const App = () => {
+  const name = "Ella";
   return (
     <div className="App">
       <h1>Hello, React!</h1>
+      {name ? (
+        <>
+          <h1>This is {name}!</h1>
+        </>
+      ) : (
+        <>
+          <h1>test</h1>
+          <h2>There is no name</h2>
+        </>
+      )}
     </div>
   );
 };
